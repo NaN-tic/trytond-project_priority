ca46080a0bf02ff9fba4238ecf479fbda3b3d0a4
Raimon Esteve <raimon@nan-tic.com>
2016-05-20 11:19:00 +0200

    Add python 3 support

--- work.py
+++ work.py
@@ -1,16 +1,14 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 from trytond.model import fields
 from trytond.pool import PoolMeta
 
-__metaclass__ = PoolMeta
 __all__ = ['Work']
 
 
 class Work:
+    __metaclass__ = PoolMeta
     __name__ = 'project.work'
-
     priority = fields.Selection([
             ('100', 'Very Low'),
             ('250', 'Low'),
